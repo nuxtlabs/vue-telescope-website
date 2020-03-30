432863b59f8af935e795bdd809f66036d4ddc812
Sébastien Chopin <seb@nuxtjs.com>
2020-03-30 17:29:16 +0200

    chore: add simple form

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,40 @@
 <template>
-  <div>Hello world</div>
+  <div>
+    <h1 class="text-3xl">Vue Telemetry</h1>
+    <form @submit.prevent="analyze">
+      <input v-model="url" type="text" class="border" />
+      <button type="submit" class="bg-green-200 px-4" :disabled="pending">
+        {{ pending ? 'Scanning...' : 'scan' }}
+      </button>
+    </form>
+    <pre v-if="result">{{ result }}</pre>
+    <div v-if="error" class="text-red-700">{{ error }}</div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      url: 'https://nuxtjs.org',
+      pending: false,
+      result: null,
+      error: null,
+    }
+  },
+  methods: {
+    async analyze() {
+      if (this.pending) return
+      this.pending = true
+      try {
+        this.result = await this.$http.$get(`api/analyze?url=${this.url}`)
+      } catch (err) {
+        this.error = err.message
+      }
+      this.pending = false
+    },
+  },
+}
 </script>
 
 <style></style>
