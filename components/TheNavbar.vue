989ec408b08382385cc924d626af8a2835941d9b
flrnt <florent@nuxtjs.com>
2020-04-03 10:46:28 +0200

    fix: fix home link

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -45,10 +45,10 @@
             />
           </div>
           <div class="hidden sm:ml-6 sm:flex">
-            <a
-              href="#"
+            <nuxt-link
+              to="/"
               class="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out"
-              >Home</a
+              >Home</nuxt-link
             >
             <nuxt-link
               to="/showcases"
@@ -87,10 +87,10 @@
     </div>
     <div class="hidden sm:hidden">
       <div class="pt-2 pb-4">
-        <a
-          href="#"
+        <nuxt-link
+          to="/"
           class="block pl-3 pr-4 py-2 border-l-4 border-indigo-500 text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out"
-          >Home</a
+          >Home</nuxt-link
         >
         <nuxt-link
           to="/showcases"
