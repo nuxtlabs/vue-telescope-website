568e232962ba1b024dc01be2b8e941793b049fa0
flrnt <florent@nuxtjs.com>
2020-04-06 17:59:06 +0200

    improvement: add netlify + heroku preview deploys to blacklist domain

--- functions/analyze.js
+++ functions/analyze.js
@@ -123,6 +123,6 @@ function slugify(domain) {
 }
 
 async function isBlacklisted(hname) {
-  const hostnameBlacklist = /((local|dev(elopment)?|stag(e|ing)?|test(ing)?|demo(shop)?|admin|google|cache)\.|\/admin|\.local|localhost)/
+  const hostnameBlacklist = /((local|dev(elopment)?|stag(e|ing)?|test(ing)?|demo(shop)?|admin|google|cache)\.|pr(eview)?-[0-9]{1,}|\/admin|\.local|localhost)/
   return hostnameBlacklist.test(hname)
 }
