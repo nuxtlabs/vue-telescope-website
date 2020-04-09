62ce70088a5a0e351ca4653a742f1be46afe8c20
flrnt <florent@nuxtjs.com>
2020-04-09 18:49:09 +0200

    chore: add tailwindUI to scan form input

--- pages/scan.vue
+++ pages/scan.vue
@@ -1,23 +1,56 @@
 <template>
   <div>
     <form @submit.prevent="analyze">
-      <input
-        v-model="url"
-        type="text"
-        class="border"
-        placeholder="Enter a domain"
-      />
-      <button type="submit" class="bg-green-200 px-4" :disabled="pending">
-        {{ pending ? 'Scanning...' : 'scan' }}
-      </button>
+      <div>
+        <label
+          for="domain"
+          class="block text-sm font-medium leading-5 text-gray-700"
+          >Enter a domain</label
+        >
+        <div class="mt-1 relative rounded-md shadow-sm">
+          <div
+            class="absolute inset-y-0 left-0 pl-2 flex items-center pointer-events-none"
+          >
+            <span class="text-gray-500 sm:text-sm sm:leading-5">https://</span>
+          </div>
+          <input
+            id="domain"
+            v-model="url"
+            type="text"
+            class="form-input block w-full pl-16 sm:pl-14 sm:text-sm sm:leading-5"
+            placeholder="www.example.com"
+          />
+          <button
+            :disabled="pending"
+            type="submit"
+            class="absolute inset-y-0 right-0 pr-2 flex items-center cursor-pointer"
+          >
+            <span class="text-gray-500 sm:text-sm sm:leading-5">
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                  stroke="#4A5568"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </span>
+          </button>
+        </div>
+      </div>
     </form>
     <div v-if="result" class="p-4 bg-gray-100">
       <pre v-if="result">{{ result }}</pre>
       <img :src="result.screenshot" class="h-64 border rounded" />
     </div>
-    <div v-if="error" class="text-red-700">
-      {{ error }}
-    </div>
+    <div v-if="error" class="text-red-700">{{ error }}</div>
   </div>
 </template>
 
@@ -25,7 +58,7 @@
 export default {
   data() {
     return {
-      url: 'https://nuxtjs.org',
+      url: 'nuxtjs.org',
       pending: false,
       result: null,
       error: null
@@ -40,7 +73,9 @@ export default {
       this.error = null
       this.result = null
       try {
-        this.result = await this.$http.$get(`api/analyze?url=${this.url}`)
+        this.result = await this.$http.$get(
+          `api/analyze?url=https://${this.url}`
+        )
       } catch (err) {
         if (err.response) {
           const { message } = await err.response.json()
