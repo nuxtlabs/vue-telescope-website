05390a9ee985c2cf36e75467b65a25d7743da8e4
flrnt <florent@nuxtjs.com>
2020-04-14 14:13:35 +0200

    chore: change navbar

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,10 +1,11 @@
 <template>
-  <nav class="bg-white shadow">
+  <nav class="bg-white">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-      <div class="relative flex justify-between h-16">
-        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+      <div class="relative flex items-center justify-between h-16">
+        <div class="absolute inset-y-0 right-0 flex items-center sm:hidden">
           <button
-            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+            class="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-green-400 hover:bg-green-50 focus:outline-none focus:bg-green-50 focus:text-green-400 transition duration-150 ease-in-out"
+            @click="open = !open"
           >
             <svg
               class="h-6 w-6"
@@ -13,14 +14,14 @@
               viewBox="0 0 24 24"
             >
               <path
-                class="inline-flex"
+                :class="{ hidden: open, 'inline-flex': !open }"
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
                 d="M4 6h16M4 12h16M4 18h16"
               />
               <path
-                class="hidden"
+                :class="{ hidden: !open, 'inline-flex': open }"
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
@@ -30,91 +31,57 @@
           </button>
         </div>
         <div
-          class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start"
+          class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-between"
         >
           <div class="flex-shrink-0 flex items-center">
-            <img
-              class="block lg:hidden h-8 w-auto"
-              src="/img/logos/workflow-mark-on-white.svg"
-              alt
-            />
-            <img
-              class="hidden lg:block h-8 w-auto"
-              src="/img/logos/workflow-logo-on-white.svg"
-              alt
-            />
-          </div>
-          <div class="hidden sm:ml-6 sm:flex">
-            <nuxt-link
-              to="/"
-              class="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out"
-            >
-              Home
-            </nuxt-link>
-            <nuxt-link
-              to="/showcases"
-              class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
-            >
-              Showcases
-            </nuxt-link>
-            <nuxt-link
-              to="/scan"
-              class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
-            >
-              Scan your site
+            <nuxt-link to="/" class="font-bold cursor-pointer">
+              <span class="text-green-400">VUE</span>
+              <span class="text-gray-700">TELEMETRY</span>
             </nuxt-link>
           </div>
-        </div>
-        <div
-          class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
-        >
-          <button
-            class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:text-gray-500 focus:bg-gray-100 transition duration-150 ease-in-out"
-          >
-            <svg
-              class="h-6 w-6"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-          </button>
+          <div :class="{ hidden: !open }" class="hidden sm:block sm:ml-6">
+            <div class="flex">
+              <nuxt-link
+                v-for="(link, id) in links"
+                :key="id"
+                :to="link.to"
+                active-class="text-green-400 bg-green-50"
+                class="ml-4 px-3 py-2 rounded-md text-base font-medium leading-5 text-gray-700 hover:text-green-400 hover:bg-green-50 focus:outline-none focus:text-green-400 focus:bg-green-50 transition duration-150 ease-in-out"
+                >{{ link.label }}</nuxt-link
+              >
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    <div class="hidden sm:hidden">
-      <div class="pt-2 pb-4">
+    <div :class="{ hidden: !open }" class="block sm:hidden">
+      <div class="px-2 pt-2 pb-3">
         <nuxt-link
-          to="/"
-          class="block pl-3 pr-4 py-2 border-l-4 border-indigo-500 text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out"
+          v-for="(link, id) in links"
+          :key="id"
+          :to="link.to"
+          active-class="text-green-400 bg-green-50"
+          class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-green-400 hover:bg-green-50 focus:outline-none focus:text-green-400 focus:bg-green-50 transition duration-150 ease-in-out"
+          >{{ link.label }}</nuxt-link
         >
-          Home
-        </nuxt-link>
-        <nuxt-link
-          to="/showcases"
-          class="mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out"
-        >
-          Showcases
-        </nuxt-link>
-        <nuxt-link
-          to="/scan"
-          class="mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out"
-        >
-          Scan your site
-        </nuxt-link>
       </div>
     </div>
   </nav>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      open: false,
+      links: [
+        { to: '/showcases', label: 'Showcases' },
+        { to: '/download', label: 'Download' },
+        { to: '/scan', label: 'Scan' }
+      ]
+    }
+  }
+}
 </script>
 
 <style></style>
