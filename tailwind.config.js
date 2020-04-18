328cee8e6cab3a7329aaa0be9501e07fefbabab3
flrnt <florent@nuxtjs.com>
2020-04-18 16:05:16 +0200

    chore: fix font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
       },
       borderRadius: {
         xl: '1rem'
