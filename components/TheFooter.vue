52c372474d7405ab03f728cfe52b831027921477
flrnt <florent@nuxtjs.com>
2020-05-06 17:55:49 +0200

    improvement: delete commented div + useless margin

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -6,7 +6,7 @@
       <div class="flex justify-center md:order-2">
         <a
           href="https://twitter.com/vuejs"
-          class="ml-6 text-gray-400 hover:text-gray-500"
+          class="text-gray-400 hover:text-gray-500"
           target="_blank"
         >
           <span class="sr-only">Twitter</span>
@@ -32,134 +32,11 @@
         </a>
       </div>
       <div class="flex mt-8 md:mt-0 md:order-1 items-center justify-center">
-        <div class="flex ’text-base leading-6 text-gray-400 items-center">
-          <svg viewBox="0 0 400 400" width="20" height="20" class="mr-2">
-            <g fill-rule="nonzero" transform="translate(0 50)" fill="none">
-              <path
-                d="M227.92099 83.45116l-13.6889 24.10141-46.8148-82.44693L23.7037 278.17052h97.3037c0 13.31084 10.61252 24.10142 23.70371 24.10142H23.70371c-8.46771 0-16.29145-4.59601-20.5246-12.05272-4.23315-7.4567-4.23272-16.64312.00114-24.0994L146.89383 13.05492c4.23415-7.45738 12.0596-12.05138 20.5284-12.05138 8.46878 0 16.29423 4.594 20.52839 12.05138l39.97037 70.39623z"
-                fill="#00C58E"
-              />
-              <path
-                d="M331.6642 266.11981l-90.05432-158.56724-13.6889-24.10141-13.68888 24.10141-90.04445 158.56724c-4.23385 7.45629-4.23428 16.64271-.00113 24.09941 4.23314 7.4567 12.05689 12.05272 20.5246 12.05272h166.4c8.46946 0 16.29644-4.591 20.532-12.04837 4.23555-7.45736 4.23606-16.64592.00132-24.10376h.01976zM144.7111 278.17052L227.921 131.65399l83.19012 146.51653h-166.4z"
-                fill="#2F495E"
-              />
-              <path
-                d="M396.04938 290.22123c-4.23344 7.45557-12.05656 12.0507-20.52345 12.0507H311.1111c13.0912 0 23.7037-10.79057 23.7037-24.10141h40.66173L260.09877 74.98553l-18.4889 32.56704L227.921 83.45116l11.65432-20.51634c4.23416-7.45738 12.0596-12.05138 20.5284-12.05138 8.46879 0 16.29423 4.594 20.52839 12.05138l115.41728 203.185c4.23426 7.457 4.23426 16.6444 0 24.1014z"
-                fill="#108775"
-              />
-            </g>
-          </svg>
+        <div class="flex items-center text-base leading-6 text-gray-400">
           <div>&copy; 2020, NuxtJS Team for VueJS Community</div>
         </div>
       </div>
     </div>
-    <!--div class="max-w-screen-xl mx-auto py-6 px-4 sm:px-6 lg:py-8 lg:px-8">
-      <div class="xl:grid xl:grid-cols-3 xl:gap-8">
-        <div class="xl:col-span-1">
-          <div class="flex-shrink-0 flex items-center">
-            <nuxt-link to="/" class="font-bold cursor-pointer">
-              <span class="text-nuxt-lightgreen">VUE</span>
-              <span class="text-nuxt-gray">TELEMETRY</span>
-            </nuxt-link>
-          </div>
-          <p class="mt-2 text-gray-500 text-base leading-6">
-            With Vue Telemetry, reveal the Vue technologies used on any website.
-          </p>
-        </div>
-        <div class="mt-6 grid grid-cols-3 gap-8 xl:mt-0 xl:col-span-2">
-          <div>
-            <h4
-              class="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase"
-            >
-              VT
-            </h4>
-            <ul class="mt-2">
-              <li v-for="(link, i) in links" :key="i" class="mt-2">
-                <nuxt-link
-                  :to="link.to"
-                  class="text-base leading-6 text-gray-500 hover:text-nuxt-lightgreen"
-                >
-                  {{ link.label }}
-                </nuxt-link>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h4
-              class="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase"
-            >
-              VueJS
-            </h4>
-            <ul class="mt-2">
-              <li>
-                <a
-                  href="https://vuejs.org/?ref=vue-telemetry"
-                  target="_blank"
-                  class="text-base leading-6 text-gray-500 hover:text-nuxt-lightgreen"
-                >
-                  Website
-                </a>
-              </li>
-              <li class="mt-2">
-                <a
-                  href="#"
-                  class="text-base leading-6 text-gray-500 hover:text-nuxt-lightgreen"
-                >
-                  Github
-                </a>
-              </li>
-              <li class="mt-2">
-                <a
-                  href="#"
-                  class="text-base leading-6 text-gray-500 hover:text-nuxt-lightgreen"
-                >
-                  Twitter
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h4
-              class="text-sm leading-5 font-semibold tracking-wider text-gray-400 uppercase"
-            >
-              NuxtJS
-            </h4>
-            <ul class="mt-2">
-              <li>
-                <a
-                  href="https://nuxtjs.org/?ref=vue-telemetry"
-                  target="_blank"
-                  class="text-base leading-6 text-gray-500 hover:text-nuxt-lightgreen"
-                >
-                  Website
-                </a>
-              </li>
-              <li class="mt-2">
-                <a
-                  href="#"
-                  class="text-base leading-6 text-gray-500 hover:text-nuxt-lightgreen"
-                >
-                  Github
-                </a>
-              </li>
-              <li class="mt-2">
-                <a
-                  href="#"
-                  class="text-base leading-6 text-gray-500 hover:text-nuxt-lightgreen"
-                >
-                  Twitter
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="mt-6 border-t border-gray-200 pt-6">
-        <p class="text-sm leading-4 text-gray-400 xl:text-center">
-          &copy; 2020, made by NuxtJS Team for the Vue Community
-        </p>
-      </div>
-    </div-->
   </div>
 </template>
 
