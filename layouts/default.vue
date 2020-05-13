3d74df504c5214e0ad2265d6673e3284e00f7db9
flrnt <florent@nuxtjs.com>
2020-05-13 10:43:02 +0200

    feat: add custom error page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="antialiased relative flex flex-col min-h-screen">
     <TheNavbar />
-    <div class="flex-1">
+    <div class="flex-1 flex flex-col">
       <nuxt />
     </div>
     <TheFooter />
---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex-1 flex flex-col items-center justify-center">
+    <p class="text-nuxt-lightgreen text-7xl leading-none font-black">{{ error.statusCode }}</p>
+    <h1 class="text-nuxt-gray text-3xl font-extrabold">{{ error.message }}</h1>
+    <nuxt-link
+      :to="{ name: 'index' }"
+      class="block mt-8 underline hover:text-nuxt-lightgreen transition ease-linear duration-150"
+    >Back to the home page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+</style>
