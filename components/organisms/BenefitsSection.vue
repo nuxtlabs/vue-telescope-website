e030f51896987d0eca77ffc139320a269598f198
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2020-07-27 18:57:46 +0300

    DEV-261: Benefits section

--- components/organisms/BenefitsSection.vue
+++ components/organisms/BenefitsSection.vue
@@ -6,14 +6,39 @@
       class="flex flex-wrap mb-24 max-w-pretty-wide m-auto"
       :class="[!(benefit.id % 2) && 'flex-row-reverse']"
     >
-      <div class="w-full md:w-1/2 px-4">
-        <h3 class="text-four leading-four font-display-weight mb-4">
+      <div class="w-full md:w-1/2 px-4 flex flex-col justify-center">
+        <h3
+          :class="[benefit.styles && benefit.styles.text]"
+          class="text-four leading-four font-display-weight mb-2"
+        >
           {{ benefit.title }}
         </h3>
-        <p class="text-eight leading-eight">{{ benefit.description }}</p>
+        <p class="text-eight leading-eight mb-4">{{ benefit.description }}</p>
+        <div class="flex flex-col mb-4">
+          <template v-for="(link, i) in benefit.links">
+            <a
+              v-if="link.type === 'external'"
+              :key="i"
+              :class="[benefit.styles && benefit.styles.text]"
+              :href="link.href"
+              class="font-bold-body-weight"
+            >
+              {{ link.text }} →
+            </a>
+            <NuxtLink
+              v-else
+              :key="i"
+              :class="[benefit.styles && benefit.styles.text]"
+              :to="link.route"
+              class="font-bold-body-weight"
+            >
+              {{ link.text }} →
+            </NuxtLink>
+          </template>
+        </div>
       </div>
-      <div class="w-full md:w-1/2 h-24 px-4">
-        <div class="bg-grey-300 w-full h-full"></div>
+      <div class="w-full md:w-1/2 px-4">
+        <img :src="benefit.image" alt="" />
       </div>
     </div>
   </section>
@@ -26,27 +51,82 @@ export default {
       benefits: [
         {
           id: 1,
-          title: 'Free',
+          title: 'Free To Use',
           description:
-            'The application and the showcase are available online and access is free for everyone.'
+            'Made for the Vue community by the Vue community! Join other Vue.js devs and explore websites for free.',
+          links: [
+            {
+              type: 'internal',
+              text: 'Explore for free',
+              route: {
+                name: 'explore'
+              }
+            }
+          ],
+          styles: {
+            text: 'text-red-500'
+          },
+          image: '/images/benefits/free.png'
         },
         {
           id: 2,
-          title: 'Browser extension',
+          title: 'Browser Extension',
           description:
-            'The application and the showcase are available online and access is free for everyone.'
+            'With the Vue Telemetry Browser extension available for both Firefox and Chrome you can see information about the website without leaving the page! Just by clicking on the extention icon and you will see what framework the page is using, what UI/CSS library and so much more.',
+          links: [
+            {
+              type: 'external',
+              href:
+                'https://github.com/nuxt-company/vue-telemetry-extensions/blob/master/README.md#chrome',
+              text: 'Check on Chrome Web Store'
+            }
+          ],
+          styles: {
+            text: 'text-orange-500'
+          },
+          image: '/images/benefits/extension.png'
         },
         {
           id: 3,
-          title: 'Open source',
+          title: 'Open Source',
           description:
-            'The application and the showcase are available online and access is free for everyone.'
+            'Vue Telemetry is open source which means anyone can contribute as well as add new plugins.',
+          links: [
+            {
+              type: 'external',
+              href: 'https://github.com/nuxt-company/vue-telemetry-analyzer',
+              text: 'Vue Telemetry Analyzer on Github'
+            },
+            {
+              type: 'external',
+              href: 'https://github.com/nuxt-company/vue-telemetry-extensions',
+              text: 'Browser extension on Github'
+            }
+          ],
+          styles: {
+            text: 'text-blue-500'
+          },
+          image: '/images/benefits/open-source.png'
         },
         {
           id: 4,
-          title: 'Huge database',
+          title: 'Huge Database',
           description:
-            'The application and the showcase are available online and access is free for everyone.'
+            // eslint-disable-next-line
+            "Wonder what other sites are using Vue.js and it's ecosystem? You can use our database, that already has 4000 websites and counting!",
+          links: [
+            {
+              type: 'internal',
+              text: 'Check our database',
+              route: {
+                name: 'explore'
+              }
+            }
+          ],
+          styles: {
+            text: 'text-primary-500'
+          },
+          image: '/images/benefits/database.png'
         }
       ]
     }
