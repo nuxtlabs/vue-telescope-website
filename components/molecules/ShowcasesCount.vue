2ba80495e68793ac97de25456d2d8d88d8cc99d8
Sébastien Chopin <seb@nuxtjs.com>
2020-10-20 14:35:05 +0200

    chore: simplify showcasesCount

--- components/molecules/ShowcasesCount.vue
+++ components/molecules/ShowcasesCount.vue
@@ -1,21 +1,9 @@
 <template>
-  <span>{{ totalShowcases || showcasesCount }}</span>
+  <span>{{ $store.state.showcasesCount }}</span>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-
 export default {
-  data() {
-    return {
-      totalShowcases: 0
-    }
-  },
-  computed: {
-    ...mapState({
-      showcasesCount: (state) => state.showcasesCount
-    })
-  },
   mounted() {
     window.onNuxtReady(() => this.$store.dispatch('GET_SHOWCASES_COUNT'))
   }
