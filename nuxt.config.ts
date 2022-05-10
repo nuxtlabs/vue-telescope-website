b34849b4afb49e4a32139575cfb5393747937cc2
Serhii <bedrytskyi@gmail.com>
2022-05-10 12:42:33 +0300

    add static assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      bodyAttrs: {
+        // TODO: does not work
+        class: [
+          'font-body antialiased overflow-x-hidden overflow-y-scroll text-grey-900 min-w-body'
+        ]
+      }
+    }
+  },
   buildModules: ['@nuxtjs/tailwindcss'],
   tailwindcss: {
     configPath: 'tailwind.config.ts'
