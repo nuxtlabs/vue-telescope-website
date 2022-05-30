dfb19bd3ceaf2dd8d2e074829277afa4bce04a2f
Yaël Guilloux <yael.guilloux@gmail.com>
2022-05-30 20:46:33 +0200

    fix(direct-hit): update direct-hit plugin

--- pages/explore/[website].vue
+++ pages/explore/[website].vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    explore-website {{ $directHit }}
-    <div v-if="$directHit" class="px-4 xl:px-8">
-      wtf {{ $directHit }}
+    explore-website {{ directHit }}
+
+    <div v-if="directHit" class="px-4 xl:px-8">
+      wtf {{ directHit }}
+
       <ShowcaseSection
         :website="website"
         class="max-w-readable xl:max-w-container m-auto"
@@ -15,6 +17,7 @@
 
 <script setup lang="ts">
 const route = useRoute()
+const { $directHit: directHit } = useNuxtApp()
 const { findOne } = useStrapi4()
 
 const { data: website } = await useAsyncData(
--- plugins/direct-hit.ts
+++ plugins/direct-hit.ts
@@ -1,12 +1,10 @@
-import { defineNuxtPlugin, ref, watch } from '#imports'
+import { defineNuxtPlugin, useState } from '#imports'
 
 export default defineNuxtPlugin((ctx: any) => {
-  const directHit = ref(true)
+  const directHit = useState('direct-hit-status', () => true)
 
   const once = ctx.$router.afterEach((to, from) => {
-    console.log('to', to)
-    console.log('from', from)
-    if (from.name !== to.name) {
+    if (from.path !== to.path) {
       directHit.value = false
       once()
     }
