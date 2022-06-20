4697fbcbdbe58ecbe250993895429b1b737948f2
Serhii <bedrytskyi@gmail.com>
2022-06-20 15:37:59 +0300

    feat: Vue 3 filter

--- components/ShowcasesSearchFilters.vue
+++ components/ShowcasesSearchFilters.vue
@@ -1,10 +1,29 @@
 <template>
   <div class="px-2">
-    {{ selectedFilters }}
     <ShowcasesSearchInput class="mb-4" @searching="$emit('searching')" />
+    <div class="mb-4 flex items-center">
+      <div class="mr-2 text-grey-700">Vue 3 only</div>
+      <AppSwitch
+        :value="selectedFilters.vueVersion_gte ? true : false"
+        @input="setVueThreeOnly($event)"
+      />
+    </div>
+
+    {{ selectedFilters }}
   </div>
 </template>
 
 <script setup lang="ts">
-const { selectedFilters } = useFilters()
+const { selectedFilters, setFilterKey, deleteFilterKey } = useFilters()
+
+function setVueThreeOnly(value) {
+  if (value) {
+    setFilterKey({
+      key: 'vueVersion_gte',
+      value: 3
+    })
+  } else {
+    deleteFilterKey('vueVersion_gte')
+  }
+}
 </script>
