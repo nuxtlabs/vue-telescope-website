9c0fa3987a5dc388f7cc534326f8ecdec47e9b87
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-07-04 14:24:19 +0200

    chore(technologies): fetch globally

--- app.vue
+++ app.vue
@@ -6,6 +6,10 @@
   </div>
 </template>
 
+<script setup lang="ts">
+await useTechnologies()
+</script>
+
 <style lang="postcss">
 body {
   @apply font-body antialiased overflow-x-hidden overflow-y-scroll text-grey-900 min-w-body;
