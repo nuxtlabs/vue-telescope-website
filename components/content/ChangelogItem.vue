1b7ab1f6ab9b4f2a603d1c9513e5368b0633b419
Serhii <bedrytskyi@gmail.com>
2022-07-29 14:51:41 +0300

    fix: Vercel env

--- components/content/ChangelogItem.vue
+++ components/content/ChangelogItem.vue
@@ -21,7 +21,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const props = defineProps({
   project: {
     type: String,
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,9 +15,9 @@ export default defineNuxtConfig({
     }
   },
   publicRuntimeConfig: {
-    baseUrl: (process.env.CONTEXT === 'production'
-      ? process.env.URL
-      : process.env.DEPLOY_PRIME_URL) || 'http://localhost:3000',
+    baseUrl: process.env.VERCEL
+      ? process.env.VERCEL_URL
+      : 'http://localhost:3000',
     strapiURL: process.env.STRAPI_URL || 'http://localhost:1337',
     serviceURL: process.env.SERVICE_URL || 'http://localhost:3001',
     iconsURL: process.env.ICONS_URL,
