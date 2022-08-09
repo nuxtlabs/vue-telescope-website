9410cdc66cee3693fd4b14129b73671270340bc3
Serhii <bedrytskyi@gmail.com>
2022-08-09 19:15:41 +0300

    feat: error page

---
+++ components/ErrorPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex-1 flex flex-col items-center justify-center">
+    <img src="/404.svg" class="md:max-w-[75vw]">
+
+    <h2 class="text-grey-900 text-six leading-six font-display-weight mb-4">
+      {{ error.message }}
+    </h2>
+
+    <NuxtLink
+      :to="{ name: 'index' }"
+      class="block text-primary-500 has-hover:hover:underline has-hover:hover:opacity-75 font-bold-body-weight"
+    >
+      Back to the home page
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  error: {
+    type: Object,
+    default: () => ({})
+  }
+})
+</script>
---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <ErrorPage :error="error" />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  error: {
+    type: Object,
+    required: true
+  }
+})
+</script>
