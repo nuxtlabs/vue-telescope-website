94dc046c0d93eb868c1a1b36f307c41f42b128d5
Sébastien Chopin <seb@nuxtjs.com>
2022-08-12 23:41:52 +0200

    chore: update to an hour

--- server/middleware/swr.ts
+++ server/middleware/swr.ts
@@ -1,5 +1,6 @@
 export default eventHandler((event) => {
   if (process.env.NODE_ENV === 'production') {
-    event.res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate=80')
+    // Cache for 1 hour
+    event.res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate=3599')
   }
 })
