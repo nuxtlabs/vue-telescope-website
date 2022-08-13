35e4dc050716d86a8eb1c9222046ddf3cabebaa6
Sébastien Chopin <seb@nuxtjs.com>
2022-08-13 13:59:12 +0200

    chore: try longer cache

--- server/middleware/swr.ts
+++ server/middleware/swr.ts
@@ -2,8 +2,8 @@ export default eventHandler((event) => {
   if (process.env.NODE_ENV === 'production') {
     // Cache for 1 hour
     handleCacheHeaders(event, {
-      maxAge: 1,
-      cacheControls: ['stale-while-revalidate=3599']
+      maxAge: 3540,
+      cacheControls: ['stale-while-revalidate=60']
     })
   }
 })
