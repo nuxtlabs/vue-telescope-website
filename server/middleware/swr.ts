2c4ccb13f26cbee6d949cf2e73c9e07abce2b8fa
Sébastien Chopin <seb@nuxtjs.com>
2022-08-13 10:42:30 +0200

    chore: use handleCacheHeaders

--- server/middleware/swr.ts
+++ server/middleware/swr.ts
@@ -1,6 +1,9 @@
 export default eventHandler((event) => {
   if (process.env.NODE_ENV === 'production') {
     // Cache for 1 hour
-    event.res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate=3599')
+    handleCacheHeaders(event, {
+      maxAge: 1,
+      cacheControls: ['stale-while-revalidate=3599']
+    })
   }
 })
